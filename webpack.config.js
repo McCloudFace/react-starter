5840eb13fbb53c771bbc08edf3c816598723458f
Jeff Chasin <McCloudFace1@gmail.com>
2018-11-06 18:58:16 -0500

    eslint tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
         use: ['babel-loader']
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       }
